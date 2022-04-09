affaee833007d7e0a5972bf00b96b1aa64175d55
oziuzoma <99350367+oziuzoma@users.noreply.github.com>
2022-04-09 16:17:19 +0000

    Update example.tf

--- example.tf
+++ example.tf
@@ -2,8 +2,8 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "" 
-  secret_key = ""
+  access_key = "var.access_key" 
+  secret_key = "var.secret_key"
 }
 
 # Create a VPC 
